232038e191c6f856fdadc973833da1884a902a5d
Zmetser <zee@thezee.hu>
2013-06-03 16:45:32 +0200

    add demo

---
+++ demo/demo_v2.js
@@ -0,0 +1,56 @@
+/*
+    This demo is for Redactor v9.0
+ */
+
+$(document).ready( function () {
+    var textarea = $('#redactor');
+    var placeholders = [
+        {
+            "name": "user_name",
+            "value": "User name"
+        },
+        {
+            "name": "commit_message",
+            "value": "commit message comes here"
+        },
+        {
+            "name": "commit_summary",
+            "value": "commit summary"
+        },
+        {
+            "name": "repo_url",
+            "value": "repository"
+        },
+        {
+            "name": "repo_options",
+            "value": "repository options"
+        },
+        {
+            "name": "file_changes",
+            "value": "file changes"
+        },
+        {
+            "name": "signature",
+            "value": "Signature"
+        }
+    ];
+
+    // Add placeholders for the textarea
+    textarea.data('placeholders', placeholders);
+
+    // Create redactor instance
+    textarea.redactor({
+        plugins: ['placeholders'], // Load placeholder plugin
+
+        // Nothing required below this line
+        shortcuts: true,
+        initCallback: function () {
+            var le_button = $('.redactor_btn_placeholders');
+            le_button.tooltip({
+                trigger: 'manual',
+                placement: 'left'
+            });
+            le_button.tooltip('show');
+        }
+    });
+});
